a89b4408d58e58e2c94002869b76bb9f4386daff
Tong Chen <tongxyh@163.com>
2017-12-07 13:49:30 +0800

    ssim function modified

--- l8-test.py
+++ l8-test.py
@@ -17,6 +17,7 @@ from keras import backend as K
 from keras import optimizers
 
 import matplotlib.pyplot as plt
+import cv2
 import numpy
 import math
 import h5py
@@ -25,6 +26,7 @@ def psnr(y_true, y_pred):
     mse = K.mean(K.square(y_true[:,:,0] - y_pred[:,:,0]), axis=(-3, -2))
     mse=K.mean(mse)
     return 20 * K.log(1. / K.sqrt(mse)) / numpy.log(10)#Attention,if you normalization pixels ,you use 1 not 255
+
 def ssim(y_true, y_pred):#may be wrong
     K1 = 0.04
     K2 = 0.06
@@ -32,7 +34,7 @@ def ssim(y_true, y_pred):#may be wrong
     mu_y = K.mean(y_true)
     sig_x = K.std(y_pred)
     sig_y = K.std(y_true)
-    sig_xy = (sig_x * sig_y) ** 0.5
+    sig_xy = K.mean(y_pred * y_true) - mu_x * mu_y
     L =  255
     C1 = (K1 * L) ** 2
     C2 = (K2 * L) ** 2
@@ -46,14 +48,13 @@ def create_model(img_height,img_width,img_channel):
     L3 = Conv2D(64, (3, 3), padding='same', activation='relu', kernel_initializer='glorot_uniform')(L2)
     L4 = Conv2D(64, (3, 3), padding='same', activation='relu', kernel_initializer='glorot_uniform')(L3)
     L4=concatenate([L4,L1],axis=-1)
+    #L4 = BatchNormalization(momentum=bn_num)(L4)
     L5 = Conv2D(64, (1, 1), padding='same', activation='relu', kernel_initializer='glorot_uniform')(L4)
     L6 = Conv2D(64, (5, 5), padding='same', activation='relu', kernel_initializer='glorot_uniform')(L5)
     L6=concatenate([L6,L1],axis=-1)
     L7 = Conv2D(128, (1, 1), padding='same', activation='relu', kernel_initializer='glorot_uniform')(L6)
-    L8 = Conv2D(img_channel, (5, 5), padding='same', activation='relu, kernel_initializer='glorot_uniform')(L7)
+    L8 = Conv2D(img_channel, (5, 5), padding='same', activation='relu', kernel_initializer='glorot_uniform')(L7)
     deblocking =Model(inputs=ip,outputs= L8)
-    optimizer = optimizers.Adam(lr=1e-4)
-    deblocking.compile(optimizer=optimizer,loss='mean_squared_error', metrics=[psnr,ssim])
     return deblocking
 
 '''
@@ -61,16 +62,21 @@ from keras.utils import plot_model
 plot_model(deblocking, to_file='model1.png', show_shapes=True, show_layer_names=True)
 '''
 def main():
-    im = plt.imread('test/QP40/433.png')
-    h,w,c = im.shape
-    im = im.reshape(1,h,w,c)/255.0
-
+    im = plt.imread('test/input/433.png')
+    h,w = im.shape
+    c = 1
+    im = im.reshape(1,h,w,c)
+    print(im.shape)
     l8 = create_model(h,w,c)
-    l8.load_weights('models/38-0.0737459.hdf5')
+    l8.load_weights('models/198-0.0008910.hdf5')
     out = l8.predict(im)
-    out = out.reshape(h,w,c)
-
-    plt.imsave('test/result/result.png',out*255.0)
+    print(out.shape)
+    out = out.reshape(h,w)
+    out[out > 1] = 1
+    out[out < 0] = 0
+    print(out.shape)
+    #plt.imsave('test/resutl/result.bmp',out)
+    cv2.imwrite('test/result/result-y.bmp',out*255.0)
 
 if __name__ == "__main__":
     main()
--- l8.py
+++ l8.py
@@ -10,31 +10,35 @@ session = tf.Session(config=config)
 
 from keras.models import Model
 from keras.callbacks import ModelCheckpoint
-from keras.layers import Input
+from keras.layers import Input,BatchNormalization
 from keras.layers.convolutional import Conv2D
 from keras.layers.merge import concatenate
 from keras import backend as K
 from keras import optimizers
+#from skimage.measure import compare_ssim as ssim
+
+import matplotlib.pyplot as plt
 import numpy
 import math
 import h5py
 
 def psnr(y_true, y_pred):
     mse = K.mean(K.square(y_true[:,:,0] - y_pred[:,:,0]), axis=(-3, -2))
-    mse=K.mean(mse)
+    mse = K.mean(mse)
     return 20 * K.log(1. / K.sqrt(mse)) / numpy.log(10)#Attention,if you normalization pixels ,you use 1 not 255
-def ssim(y_true, y_pred):#may be wrong
-    K1 = 0.04
-    K2 = 0.06
+
+def ssim(y_true, y_pred):
+    K1 = 0.04 #0.01
+    K2 = 0.06 #0.03
     mu_x = K.mean(y_pred)
     mu_y = K.mean(y_true)
     sig_x = K.std(y_pred)
     sig_y = K.std(y_true)
-    sig_xy = (sig_x * sig_y) ** 0.5
-    L =  255
+    sig_xy = K.mean(y_pred * y_true) - mu_x * mu_y
+    L =  1.0
     C1 = (K1 * L) ** 2
     C2 = (K2 * L) ** 2
-    ssim = (2 * mu_x * mu_y + C1) * (2 * sig_xy * C2) * 1.0 / ((mu_x ** 2 + mu_y ** 2 + C1) * (sig_x ** 2 + sig_y ** 2 + C2))
+    ssim = (2 * mu_x * mu_y + C1) * (2 * sig_xy + C2) * 1.0 / ((mu_x ** 2 + mu_y ** 2 + C1) * (sig_x ** 2 + sig_y ** 2 + C2))
     return ssim
 
 def create_model(img_height,img_width,img_channel):
@@ -44,14 +48,15 @@ def create_model(img_height,img_width,img_channel):
     L3 = Conv2D(64, (3, 3), padding='same', activation='relu', kernel_initializer='glorot_uniform')(L2)
     L4 = Conv2D(64, (3, 3), padding='same', activation='relu', kernel_initializer='glorot_uniform')(L3)
     L4=concatenate([L4,L1],axis=-1)
+    #L4 = BatchNormalization(momentum=0.9)(L4)
     L5 = Conv2D(64, (1, 1), padding='same', activation='relu', kernel_initializer='glorot_uniform')(L4)
     L6 = Conv2D(64, (5, 5), padding='same', activation='relu', kernel_initializer='glorot_uniform')(L5)
     L6=concatenate([L6,L1],axis=-1)
     L7 = Conv2D(128, (1, 1), padding='same', activation='relu', kernel_initializer='glorot_uniform')(L6)
-    L8 = Conv2D(img_channel, (5, 5), padding='same', activation='relu', kernel_initializer='glorot_uniform')(L7)
+    L8 = Conv2D(img_channel, (5, 5), padding='same', kernel_initializer='glorot_uniform')(L7)
     deblocking =Model(inputs=ip,outputs= L8)
     optimizer = optimizers.Adam(lr=1e-4)
-    deblocking.compile(optimizer=optimizer,loss='mean_squared_error', metrics=[psnr,ssim])
+    deblocking.compile(optimizer=optimizer,loss='mean_squared_error', metrics=[psnr,ssim)
     return deblocking
 
 '''
@@ -67,8 +72,14 @@ def main():
     test_data = file_test['data'][:].transpose(0,3,2,1)
     test_label = file_test['label'][:].transpose(0,3,2,1)
 
-    fvc_model = create_model(64,64,3)
-    checkpoint=ModelCheckpoint(filepath=r'models/0/{epoch:02d}-{val_loss:.7f}.hdf5',period=1)
+    #plt.imsave('test/result/train_in.png',train_data[1,:,:,:])
+    #plt.imsave('test/result/train_out.png',train_label[1,:,:,:])
+    #plt.imsave('test/result/test_in.png',test_data[1,:,:,:])
+    #plt.imsave('test/result/test_out.png',test_label[1,:,:,:])
+    #ssim_noise = ssim(img, img_noise,data_range=img_noise.max() - img_noise.min())
+
+    fvc_model = create_model(64,64,1)
+    checkpoint=ModelCheckpoint(filepath=r'models/1/{epoch:02d}-{val_loss:.7f}.hdf5',period=1)
     fvc_model.fit(train_data, train_label,
                     epochs=200,
                     batch_size=64,
